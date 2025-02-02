bea9e8c01415b3a7940613df8b0b5cf915c0191a
Dismatest <billclintonogot88@gmail.com>
2025-02-02 18:10:41 +0300

    table editing

--- src/pages/doctorsViews/Doctor-Forms/PatientSigns.jsx
+++ src/pages/doctorsViews/Doctor-Forms/PatientSigns.jsx
@@ -33,6 +33,26 @@ const PatientSigns = ({ treatmentNo, patientNo, moveToNextTab }) => {
     (state) => state.getPatientHistoryNotesReducer
   );
 
+  const notesType = [
+    { value: "1", label: "Chief Complaints" },
+    { value: "2", label: "Allegations" },
+    { value: "3", label: "History of Presenting Complaint" },
+    { value: "4", label: "Past Psychiatric and Medical History" },
+    { value: "5", label: "Family History" },
+    { value: "6", label: "Personal History" },
+    { value: "7", label: "Forensic History" },
+    { value: "8", label: "Premorbid Personality" },
+    { value: "9", label: "Medical" },
+    { value: "10", label: "Gynecology" },
+  ];
+
+  const filterCollapseData = data.filter((item) =>
+    notesType.map((note) => note.label).includes(item.Notes_Type)
+  );
+
+  const allNotesPresent = notesType.every((note) =>
+    filterCollapseData.some((item) => item.Notes_Type === note.label)
+  );
   const [currentStep, setCurrentStep] = useState(0);
   const [form] = Form.useForm();
   const dispatch = useDispatch();
@@ -275,9 +295,13 @@ const PatientSigns = ({ treatmentNo, patientNo, moveToNextTab }) => {
       <Typography.Text style={{ fontWeight: "bold", color: "#0f5689" }}>
         <FileOutlined /> Patient History Notes
       </Typography.Text>
-      {(role === "Doctor" || role === "Psychology") && (
+      {(role === "Doctor" || role === "Psychology") && !allNotesPresent && (
         <>
-          <Steps current={currentStep} size="small" style={{ marginBottom: 24 }}>
+          <Steps
+            current={currentStep}
+            size="small"
+            style={{ marginBottom: 24 }}
+          >
             {steps.map((step) => (
               <Step key={step.key} title={step.title} />
             ))}
@@ -299,6 +323,7 @@ const PatientSigns = ({ treatmentNo, patientNo, moveToNextTab }) => {
           </Form>
         </>
       )}
+
       <PatientHistoryNotesTable data={data} />
     </div>
   );
