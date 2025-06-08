d1ebcf046a889760d3f4cfbe20f6e18f66a61d84
Lovefrom Oliver <lvfmandi@gmail.com>
2025-06-08 23:41:01 +0300

    This will add the searching feature in the doctor notes page

--- src/pages/doctorsViews/Doctor-Forms/PatientSigns.jsx
+++ src/pages/doctorsViews/Doctor-Forms/PatientSigns.jsx
@@ -1,89 +1,24 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from "react-router-dom";
 
-import { Card, Tabs, Space, message } from 'antd';
-
-import {
-  buildFormStructure,
-  buildSelectedItemsTree,
-} from '../../../utils/doctorNotesTree';
+import { Card, Tabs, Space } from "antd";
 
 import {
   CategoriesReport,
   CategoriesReportPreview,
-} from './Consultation-Room-File/CategoriesReport';
-import { NoData } from '../../../components/NoData';
-import { Categories } from './Consultation-Room/Categories';
+} from "./Consultation-Room-File/CategoriesReport";
+import { NoData } from "../../../components/NoData";
+import { Categories } from "./Consultation-Room/Categories";
 
-import { SAVE_DOCTOR_NOTES_RESET } from '../../../actions/Doc-actions/saveDoctorNotes';
-import { getDoctorsNotesData } from '../../../actions/Doc-actions/getDoctorsNotesData';
-
-const PatientSigns = ({ treatmentNo, filter }) => {
-  const dispatch = useDispatch();
+const PatientSigns = ({ tree, searchValue, selectedTreeReport }) => {
   const location = useLocation();
+
   const { patientDetails } = location.state || {};
-
-  const { data: getDoctorNotesData } = useSelector(
-    (state) => state.getDoctorsNotesData,
-  );
-  const { data: saveDoctorNotesData, error: saveDoctorNotesError } =
-    useSelector((state) => state.saveDoctorNotes);
-  const { sections, sectionCategories, formItems } = getDoctorNotesData || {};
-
-  const filterSections = (filter) => {
-    var returnValue = [];
-    switch (filter) {
-      case 'PH':
-        returnValue = sections.filter(
-          ({ Form_Type }) => Form_Type === 'Patient History',
-        );
-        break;
-      case 'MSE':
-        returnValue = sections.filter(({ Form_Type }) => Form_Type === 'MSE');
-        break;
-      case 'PE':
-        returnValue = sections.filter(
-          ({ Form_Type }) => Form_Type === 'Physical Exam',
-        );
-        break;
-      default:
-        returnValue = sections;
-        break;
-    }
-
-    return returnValue;
-  };
-
-  const tree = Object.keys(getDoctorNotesData).length
-    ? buildFormStructure(filterSections(filter), sectionCategories, formItems)
-    : null;
-
-  const selectedTreeReport = tree
-    ? buildSelectedItemsTree(
-        filterSections(filter),
-        sectionCategories,
-        formItems,
-      )
-    : null;
-
-  // loading the doctor notes data to add
-  useEffect(() => {
-    dispatch(getDoctorsNotesData({ treatmentNo }));
-  }, [treatmentNo, dispatch, saveDoctorNotesData]);
-
-  // tracking when the adding of data has failed
-  useEffect(() => {
-    if (saveDoctorNotesError) {
-      message.error(saveDoctorNotesError);
-      dispatch({ type: SAVE_DOCTOR_NOTES_RESET });
-    }
-  }, [saveDoctorNotesError]);
+  const treatmentNo = new URLSearchParams(location.search).get("TreatmentNo");
 
   const items = tree?.map(
     ({ Section_Name: label, Section_ID: key, categories }) => {
       const matchingResultsSection = selectedTreeReport.find(
-        (section) => section.Section_ID === key,
+        (section) => section.Section_ID === key
       );
 
       return {
@@ -95,30 +30,38 @@ const PatientSigns = ({ treatmentNo, filter }) => {
             categories={categories}
             treatmentNo={treatmentNo}
             data={matchingResultsSection}
-            completed={patientDetails?.Status === 'Completed'}
+            multiple={Boolean(searchValue)}
+            completed={patientDetails?.Status === "Completed"}
           />
         ),
       };
-    },
+    }
   );
 
   return (
-    <Tabs
-      size="small"
-      items={items}
-      tabPosition={'left'}
-      className="wrap-white-space"
-      tabBarStyle={{
-        maxWidth: '270px',
-        textAlign: 'left',
-      }}
-    />
+    <>
+      {items?.length ? (
+        <Tabs
+          size="small"
+          items={items}
+          tabPosition={"left"}
+          className="wrap-white-space"
+          tabBarStyle={{
+            maxWidth: "270px",
+            textAlign: "left",
+          }}
+        />
+      ) : (
+        <NoData content="No data matched your search" />
+      )}
+    </>
   );
 };
 
 const PatientSignsTabsChild = ({
   data,
   label,
+  multiple,
   completed,
   categories,
   treatmentNo,
@@ -126,14 +69,15 @@ const PatientSignsTabsChild = ({
   return (
     <Space
       style={{
-        width: '100%',
-        display: 'grid',
-        alignItems: 'stretch',
-        gridTemplateColumns: completed ? '1fr' : 'repeat(2, 1fr)',
+        width: "100%",
+        display: "grid",
+        alignItems: "stretch",
+        gridTemplateColumns: completed ? "1fr" : "repeat(2, 1fr)",
       }}
     >
       {!completed && (
         <Categories
+          multiple={multiple}
           categories={categories}
           treatmentNo={treatmentNo}
         />
@@ -143,15 +87,12 @@ const PatientSignsTabsChild = ({
         type="inner"
         className="wrap-white-space"
         style={{
-          width: '100%',
-          height: '100%',
-          minHeight: '500px',
+          width: "100%",
+          height: "100%",
+          minHeight: "500px",
         }}
         title={
-          <h6
-            className="pt-2"
-            style={{ color: '#0f5689' }}
-          >
+          <h6 className="pt-2" style={{ color: "#0f5689" }}>
             {label} Preview Report
           </h6>
         }
@@ -163,10 +104,7 @@ const PatientSignsTabsChild = ({
               child={CategoriesReportPreview}
             />
           ) : (
-            <NoData
-              style={{ padding: '24px' }}
-              content={`No ${label}`}
-            />
+            <NoData style={{ padding: "24px" }} content={`No ${label}`} />
           )}
         </div>
       </Card>
