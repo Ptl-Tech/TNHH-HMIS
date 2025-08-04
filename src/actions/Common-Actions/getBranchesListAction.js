40bba18494d177ff38e1c33844eb01227f7c044b
Lovefrom Oliver <lvfmandi@gmail.com>
2025-08-04 11:22:50 +0300

    This will make the API similar

--- src/actions/Common-Actions/getBranchesListAction.js
+++ src/actions/Common-Actions/getBranchesListAction.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { message } from "antd";
 
-const API = "https://chiromo.potestastechnologies.net:8085/";
+const API = "https://chiromo.potestastechnologies.net:8091/";
 
 // Action Types
 export const GET_BRANCHES_LIST_REQUEST = "GET_BRANCHES_LIST_REQUEST";
